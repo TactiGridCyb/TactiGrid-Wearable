338722b73ece4f9f541c3a058b9f1bdfab7f70c0
Daniel Umansky <danny.umansky@gmail.com>
2025-03-27 02:43:00 +0200

    added wifi scan and fixed usp connection

---
+++ wifi-scan/wifi.ino
@@ -0,0 +1,58 @@
+
+#include "WiFi.h"
+#include "LilyGoLib.h"
+
+void setup() {
+  Serial.begin(115200);
+  watch.begin(&Serial);
+
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  Serial.println("Setup done");
+}
+
+void loop() {
+  Serial.println("Scan start");
+
+  int n = WiFi.scanNetworks();
+  Serial.println("Scan done");
+  if (n == 0) {
+    Serial.println("no networks found");
+  } else {
+    Serial.print(n);
+    Serial.println(" networks found");
+    Serial.println("Nr | SSID                             | RSSI | CH | Encryption");
+    for (int i = 0; i < n; ++i) {
+      Serial.printf("%2d", i + 1);
+      Serial.print(" | ");
+      Serial.printf("%-32.32s", WiFi.SSID(i).c_str());
+      Serial.print(" | ");
+      Serial.printf("%4ld", WiFi.RSSI(i));
+      Serial.print(" | ");
+      Serial.printf("%2ld", WiFi.channel(i));
+      Serial.print(" | ");
+      switch (WiFi.encryptionType(i)) {
+        case WIFI_AUTH_OPEN:            Serial.print("open"); break;
+        case WIFI_AUTH_WEP:             Serial.print("WEP"); break;
+        case WIFI_AUTH_WPA_PSK:         Serial.print("WPA"); break;
+        case WIFI_AUTH_WPA2_PSK:        Serial.print("WPA2"); break;
+        case WIFI_AUTH_WPA_WPA2_PSK:    Serial.print("WPA+WPA2"); break;
+        case WIFI_AUTH_WPA2_ENTERPRISE: Serial.print("WPA2-EAP"); break;
+        case WIFI_AUTH_WPA3_PSK:        Serial.print("WPA3"); break;
+        case WIFI_AUTH_WPA2_WPA3_PSK:   Serial.print("WPA2+WPA3"); break;
+        case WIFI_AUTH_WAPI_PSK:        Serial.print("WAPI"); break;
+        default:                        Serial.print("unknown");
+      }
+      Serial.println();
+      delay(10);
+    }
+  }
+  Serial.println("");
+
+  WiFi.scanDelete();
+
+  delay(5000);
+}
--- wifi-transfer/wifi.ino
+++ wifi-transfer/wifi.ino
@@ -1,10 +1,12 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
+#include "LilyGoLib.h"
+
 const char *networkName = "XXX";
 const char *networkPswd = "XXX";
 
-const char *udpAddress = "192.168.0.181";
+const char *udpAddress = "192.168.0.133";
 const int udpPort = 3333;
 
 boolean connected = false;
@@ -13,17 +15,24 @@ WiFiUDP udp;
 
 void setup() {
   Serial.begin(115200);
+  watch.begin(&Serial);
 
   connectToWiFi(networkName, networkPswd);
+  Serial.println("Starting to send messages");
+
 
   for (size_t i = 0; i < 10; i++)
   {
+    Serial.println("Message number " + String(i));
     udp.beginPacket(udpAddress, udpPort);
     udp.printf("Seconds since boot: %lu", millis() / 1000);
     udp.endPacket();
+    delay(500);
   }
 
+  WiFi.disconnect(true, true);
 
+  Serial.printf("Disconnected!");
 }
 
 void loop() {}
@@ -31,13 +40,15 @@ void loop() {}
 void connectToWiFi(const char *ssid, const char *pwd) {
   Serial.println("Connecting to WiFi network: " + String(ssid));
 
-  WiFi.disconnect(true);
+  WiFi.disconnect(true, true);
 
   WiFi.onEvent(WiFiEvent);
-
+  
   WiFi.begin(ssid, pwd);
 
   Serial.println("Waiting for WIFI connection...");
+  int status = WiFi.waitForConnectResult();
+  Serial.println(status);
 }
 
 void WiFiEvent(WiFiEvent_t event) {
@@ -51,10 +62,10 @@ void WiFiEvent(WiFiEvent_t event) {
       connected = true;
 
       break;
-    case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
-      Serial.println("WiFi lost connection");
-      connected = false;
-      break;
+    // case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+    //   Serial.println("WiFi lost connection");
+    //   connected = false;
+    //   break;
     default: break;
   }
 }
